03953ad95349a747c991635327676ce1f32068e0
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-08 10:26:01 -0800

    Add login user route

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
 
 describe('user routes', () => {
   beforeEach(() => {
@@ -22,9 +23,29 @@ describe('user routes', () => {
     email: 'giJoe@defense.gov',
   };
 
+  const createAndLogin = async () => {
+    const agent = request.agent(app);
+
+    const user = await UserService.create({ ...userSend });
+
+    await agent.post('/api/v1/users/sessions').send({ ...userSend });
+
+    return [agent, user];
+  };
+
   it('should create a new user', async () => {
     const user = await request(app).post('/api/v1/users').send(userSend);
 
     expect(user.body).toEqual(userReceive);
+  });
+
+  it('should log in a user', async () => {
+    await UserService.create({ ...userSend });
+
+    const message = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ ...userSend });
+
+    expect(message.body.message).toEqual(`Signed in as ${userSend.email}.`);
   });
 });
