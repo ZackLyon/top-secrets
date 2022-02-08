c0749395bb0da154c6755d0667e1c749b65c167d
Zack Lyon <zack.j.lyon@gmail.com>
2022-02-07 17:14:33 -0800

    Create secrets post route without authentication

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS secret;
+
+CREATE TABLE secret (
+  id BIGINT GENERATED ALWAYS AS IDENTITY,
+  title TEXT NOT NULL,
+  description TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+)
